05364aa852b7c68c4d5a05eba29a2e6340f328a0
Phongsathorn Kittiworapanya <Phongsathron@outlook.com>
2019-11-24 09:07:09 +0700

    Change auth roles

--- auth/src/main/java/com/depression/auth/auth/config/AuthServerOAuth2Config.java
+++ auth/src/main/java/com/depression/auth/auth/config/AuthServerOAuth2Config.java
@@ -56,7 +56,7 @@ public class AuthServerOAuth2Config extends AuthorizationServerConfigurerAdapter
                 .secret(bCryptPasswordEncoder().encode("secret"))
                 .authorizedGrantTypes("password", "authorization_code", "refresh_token", "client_credentials")
                 .scopes("read", "write")
-                .authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT", "USER", "ADMIN")
+                .authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT", "ROLE_USER", "ROLE_ADMIN", "ROLE_DOCTOR")
                 .autoApprove(true)
                 .accessTokenValiditySeconds(60*60*24*30)//Access token is only valid for 3 minutes.
                 .refreshTokenValiditySeconds(60*60*24*31);
