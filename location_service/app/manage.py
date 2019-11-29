87603a531b1b8493a64ae51d10dfaf779fbbe748
realkyr <60070075@kmitl.ac.th>
2019-11-29 23:37:06 +0700

    finish update on location service

--- location_service/app/manage.py
+++ location_service/app/manage.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
-import py_eureka_client.eureka_client as eureka_client
 import os
 import sys
+import py_eureka_client.eureka_client as eureka_client
 
 
 def main():
-    server_port = 8888
-    eureka_client.init(eureka_server="localhost",
-                   app_name="your_app_name",
-                   instance_port=server_port)
+    your_rest_server_port = 8000
+    eureka_client.init(eureka_server="http://34.66.153.219:8761/eureka/",
+                       app_name="LOCATION-SERVICE",
+                       instance_port=your_rest_server_port,
+                       instance_ip="35.225.198.249"
+                       )
+    # eureka_client.init_registry_client(eureka_server="http://34.66.153.219:8761/eureka/",
+    #                                    app_name="LOCATION_SERVICE")
+    # eureka_client.stop()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
     try:
         from django.core.management import execute_from_command_line
