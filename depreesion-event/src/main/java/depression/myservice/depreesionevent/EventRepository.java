3aa362d859ceda148ba520bbb6cd3603214e4c85
WaiWarit <33046422+WaiWarit@users.noreply.github.com>
2019-11-29 15:44:29 +0700

    addEventService

---
+++ depreesion-event/src/main/java/depression/myservice/depreesionevent/EventRepository.java
@@ -0,0 +1,8 @@
+package depression.myservice.depreesionevent;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface EventRepository extends JpaRepository<Event, Long>{
+
+}
---
+++ depreesion-event/src/test/java/depression/myservice/depreesionevent/DepreesionEventApplicationTests.java
@@ -0,0 +1,13 @@
+package depression.myservice.depreesionevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepreesionEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
