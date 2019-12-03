4b30dbfc05d98c6a1bafbf9a5b90fe5b5a2f8e69
azurelisz <33046403+azurelisz@users.noreply.github.com>
2019-12-03 19:37:28 +0700

    add external ip

--- depressed/suggest/__init__.py
+++ depressed/suggest/__init__.py
@@ -4,4 +4,5 @@ your_rest_server_port = 9090
 # The flowing code will register your server to eureka server and also start to send heartbeat every 30 seconds
 eureka_client.init(eureka_server="http://34.66.153.219:8761/eureka/",
                    app_name="suggestion_service",
-                   instance_port=your_rest_server_port)
+                   instance_port=your_rest_server_port,
+                  instance_ip="34.69.92.81/suggest/suggestion_list")
